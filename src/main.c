f9227dc5b739fc33a5cedd9e161c0c7a5a479849
matheus <matheus.costa.s.azevedo@gmail.com>
2025-06-17 17:31:41 -0400

    working professor adaptado

--- src/main.c
+++ src/main.c
@@ -1,86 +1,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
-
-#define STACKSIZE 2048
-#define PRIORITY 7
-#define STEP PWM_USEC(50)
-
-#define SERVO1 DT_ALIAS(servo1)
-#define SERVO2 DT_ALIAS(servo2)
-
-#if !DT_NODE_HAS_STATUS_OKAY(SERVO1)
-#error "Unsupported board: SERVO1 devicetree alias is not defined"
-#endif
-
-#if !DT_NODE_HAS_STATUS_OKAY(SERVO2)
-#error "Unsupported board: SERVO2 devicetree alias is not defined"
-#endif
-
-enum direction {
-	UP,
-	DOWN,
-};
-
-struct servo_config {
-	struct pwm_dt_spec pwm;
-	uint32_t pulse_width;
-	enum direction dir;
-	uint32_t min_pulse;
-	uint32_t max_pulse;
-};
-
-static struct servo_config servo1 = {
-	.pwm = PWM_DT_SPEC_GET(SERVO1),
-	.pulse_width = DT_PROP(SERVO1, min_pulse),
-	.dir = UP,
-	.min_pulse = DT_PROP(SERVO1, min_pulse),
-	.max_pulse = DT_PROP(SERVO1, max_pulse),
-};
-
-static struct servo_config servo2 = {
-	.pwm = PWM_DT_SPEC_GET(SERVO2),
-	.pulse_width = DT_PROP(SERVO2, max_pulse),
-	.dir = DOWN,
-	.min_pulse = DT_PROP(SERVO2, min_pulse),
-	.max_pulse = DT_PROP(SERVO2, max_pulse),
-};
-
-void servo_motor(void *pctx, void *unused1, void *unused2)
-{
-	struct servo_config *ctx = pctx;
-
-	while (1) {
-		pwm_set_pulse_dt(&ctx->pwm, ctx->pulse_width);
-
-		if (ctx->dir == UP) {
-			if (ctx->pulse_width + STEP >= ctx->max_pulse) {
-				ctx->pulse_width = ctx->max_pulse;
-				ctx->dir = DOWN;
-			} else {
-				ctx->pulse_width += STEP;
-			}
-		} else {
-			if (ctx->pulse_width <= ctx->min_pulse + STEP) {
-				ctx->pulse_width = ctx->min_pulse;
-				ctx->dir = UP;
-			} else {
-				ctx->pulse_width -= STEP;
-			}
-		}
-
-		k_sleep(K_MSEC(200)); 
-	}
-}
 
 int main(void)
 {
-	printk("Controle de dois servos iniciado\n");
-	
-	
-	return 0;
+    printk("Servos controlados por servo_control.c\n");
+    return 0;
 }
-
-K_THREAD_DEFINE(servo1_id, STACKSIZE, servo_motor, &servo1, NULL, NULL,	PRIORITY, 0, 0);
-K_THREAD_DEFINE(servo2_id, STACKSIZE, servo_motor, &servo2, NULL, NULL, PRIORITY, 0, 0);
---
+++ src/servo_control.c
@@ -0,0 +1,104 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(servo_control, LOG_LEVEL_INF);
+
+#define STACKSIZE 1024
+#define PRIORITY 5
+#define STEP PWM_USEC(50)
+
+#define NUM_SERVOS 4
+
+#define SERVO_ALIAS(n) DT_ALIAS(servo##n)
+
+#if !DT_NODE_HAS_STATUS_OKAY(SERVO_ALIAS(1))
+#error "Alias servo1 não definido"
+#endif
+#if !DT_NODE_HAS_STATUS_OKAY(SERVO_ALIAS(2))
+#error "Alias servo2 não definido"
+#endif
+#if !DT_NODE_HAS_STATUS_OKAY(SERVO_ALIAS(3))
+#error "Alias servo3 não definido"
+#endif
+#if !DT_NODE_HAS_STATUS_OKAY(SERVO_ALIAS(4))
+#error "Alias servo4 não definido"
+#endif
+
+enum direction {
+    UP,
+    DOWN,
+};
+
+struct servo_config {
+    struct pwm_dt_spec pwm;
+    uint32_t pulse_width;
+    enum direction dir;
+    uint32_t min_pulse;
+    uint32_t max_pulse;
+};
+
+static struct servo_config servos[NUM_SERVOS] = {
+    {
+        .pwm = PWM_DT_SPEC_GET(SERVO_ALIAS(1)),
+        .pulse_width = DT_PROP(SERVO_ALIAS(1), min_pulse),
+        .dir = UP,
+        .min_pulse = DT_PROP(SERVO_ALIAS(1), min_pulse),
+        .max_pulse = DT_PROP(SERVO_ALIAS(1), max_pulse),
+    },
+    {
+        .pwm = PWM_DT_SPEC_GET(SERVO_ALIAS(2)),
+        .pulse_width = DT_PROP(SERVO_ALIAS(2), max_pulse),
+        .dir = DOWN,
+        .min_pulse = DT_PROP(SERVO_ALIAS(2), min_pulse),
+        .max_pulse = DT_PROP(SERVO_ALIAS(2), max_pulse),
+    },
+    {
+        .pwm = PWM_DT_SPEC_GET(SERVO_ALIAS(3)),
+        .pulse_width = DT_PROP(SERVO_ALIAS(3), min_pulse),
+        .dir = UP,
+        .min_pulse = DT_PROP(SERVO_ALIAS(3), min_pulse),
+        .max_pulse = DT_PROP(SERVO_ALIAS(3), max_pulse),
+    },
+    {
+        .pwm = PWM_DT_SPEC_GET(SERVO_ALIAS(4)),
+        .pulse_width = DT_PROP(SERVO_ALIAS(4), max_pulse),
+        .dir = DOWN,
+        .min_pulse = DT_PROP(SERVO_ALIAS(4), min_pulse),
+        .max_pulse = DT_PROP(SERVO_ALIAS(4), max_pulse),
+    },
+};
+
+static void servo_motor(void *p1, void *p2, void *p3)
+{
+    struct servo_config *ctx = p1;
+
+    while (1) {
+        pwm_set_pulse_dt(&ctx->pwm, ctx->pulse_width);
+        LOG_INF("Servo ajustado para %d us", ctx->pulse_width);
+
+        if (ctx->dir == UP) {
+            if (ctx->pulse_width + STEP >= ctx->max_pulse) {
+                ctx->pulse_width = ctx->max_pulse;
+                ctx->dir = DOWN;
+            } else {
+                ctx->pulse_width += STEP;
+            }
+        } else {
+            if (ctx->pulse_width <= ctx->min_pulse + STEP) {
+                ctx->pulse_width = ctx->min_pulse;
+                ctx->dir = UP;
+            } else {
+                ctx->pulse_width -= STEP;
+            }
+        }
+
+        k_sleep(K_MSEC(200));
+    }
+}
+
+K_THREAD_DEFINE(servo1_thread_id, STACKSIZE, servo_motor, &servos[0], NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(servo2_thread_id, STACKSIZE, servo_motor, &servos[1], NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(servo3_thread_id, STACKSIZE, servo_motor, &servos[2], NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(servo4_thread_id, STACKSIZE, servo_motor, &servos[3], NULL, NULL, PRIORITY, 0, 0);
