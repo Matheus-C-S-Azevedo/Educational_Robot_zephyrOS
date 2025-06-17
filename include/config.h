bfbb92632af68ac099d268204fd128bf071f4c4f
matheus <matheus.costa.s.azevedo@gmail.com>
2025-06-17 10:57:33 -0400

    Fat: adicionando constantes globais no config.h

--- include/config.h
+++ include/config.h
@@ -4,5 +4,12 @@
 #define WIFI_SSID "LAB_I4.0"
 #define WIFI_PSK "L4B0@2025#"
 
+#define MQTT_BROKER_ADDR "192.168.0.102"
+#define MQTT_BROKER_PORT 1883
+
+#define MQTT_CLIENT_ID "zephyr_client"
+#define MQTT_TOPIC_DEFAULT "zephyr/test"
+#define MQTT_QOS MQTT_QOS_0_AT_MOST_ONCE
+
 #endif
 
