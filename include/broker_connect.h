a0f96d2512bfd15d63516d5556153f6442311d39
matheus <matheus.costa.s.azevedo@gmail.com>
2025-06-17 10:58:20 -0400

    Feat: expondo o método de conexão do broker

---
+++ include/broker_connect.h
@@ -0,0 +1,8 @@
+#ifndef BROKER_CONNECT_H
+#define BROKER_CONNECT_H
+
+#include <zephyr/net/mqtt.h>
+
+int connect_to_mqtt(struct mqtt_client *client, struct sockaddr_storage *broker);
+
+#endif // BROKER_CONNECT_H
